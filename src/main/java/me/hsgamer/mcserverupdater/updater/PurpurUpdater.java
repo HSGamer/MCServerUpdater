f3cd6b58516150b3b073660ea9ec2e443a682e7f
Huynh Tien <huynhqtienvtag@gmail.com>
2021-12-06 21:28:13 +0700

    update PurpurMC url & get numeric latest build

--- src/main/java/me/hsgamer/mcserverupdater/updater/PurpurUpdater.java
+++ src/main/java/me/hsgamer/mcserverupdater/updater/PurpurUpdater.java
@@ -4,6 +4,7 @@ import me.hsgamer.hscore.web.UserAgent;
 import me.hsgamer.hscore.web.WebUtils;
 import me.hsgamer.mcserverupdater.api.FileDigestChecksum;
 import me.hsgamer.mcserverupdater.api.InputStreamUpdater;
+import me.hsgamer.mcserverupdater.api.LatestBuild;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
@@ -11,10 +12,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URLConnection;
 import java.security.MessageDigest;
-import java.util.Locale;
 
-public class PurpurUpdater implements FileDigestChecksum, InputStreamUpdater {
-    private static final String URL = "https://api.pl3x.net/v2/purpur/";
+public class PurpurUpdater implements FileDigestChecksum, InputStreamUpdater, LatestBuild {
+    private static final String URL = "https://api.purpurmc.org/v2/purpur/";
     private static final String VERSION_URL = URL + "%s/";
     private static final String BUILD_URL = VERSION_URL + "%s/";
     private static final String DOWNLOAD_URL = BUILD_URL + "download";
@@ -26,7 +26,6 @@ public class PurpurUpdater implements FileDigestChecksum, InputStreamUpdater {
 
     @Override
     public InputStream getInputStream(String version, String build) {
-        build = build.toLowerCase(Locale.ROOT);
         String url = String.format(DOWNLOAD_URL, version, build);
         try {
             URLConnection connection = WebUtils.openConnection(url, UserAgent.CHROME);
@@ -39,7 +38,6 @@ public class PurpurUpdater implements FileDigestChecksum, InputStreamUpdater {
 
     @Override
     public String getChecksum(String version, String build) {
-        build = build.toLowerCase(Locale.ROOT);
         String url = String.format(BUILD_URL, version, build);
         try {
             URLConnection connection = WebUtils.openConnection(url, UserAgent.CHROME);
@@ -55,5 +53,20 @@ public class PurpurUpdater implements FileDigestChecksum, InputStreamUpdater {
     @Override
     public String getDefaultVersion() {
         return "1.17.1";
+    }
+
+    @Override
+    public String getLatestBuild(String version) {
+        String url = String.format(VERSION_URL, version);
+        try {
+            URLConnection connection = WebUtils.openConnection(url, UserAgent.CHROME);
+            InputStream inputStream = connection.getInputStream();
+            JSONObject jsonObject = new JSONObject(new JSONTokener(inputStream));
+            JSONObject builds = jsonObject.getJSONObject("builds");
+            return builds.getString("latest");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
