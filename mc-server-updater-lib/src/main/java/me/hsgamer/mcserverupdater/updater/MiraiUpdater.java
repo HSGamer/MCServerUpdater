e84e0a2fc291beaf57af986cbc31d0c7ad255f00
HSGamer <huynhqtienvtag@gmail.com>
2022-08-10 21:22:36 +0700

    update MiraiUpdater to use Jenkins

--- mc-server-updater-lib/src/main/java/me/hsgamer/mcserverupdater/updater/MiraiUpdater.java
+++ mc-server-updater-lib/src/main/java/me/hsgamer/mcserverupdater/updater/MiraiUpdater.java
@@ -1,21 +1,30 @@
 package me.hsgamer.mcserverupdater.updater;
 
-import me.hsgamer.mcserverupdater.api.GithubReleaseUpdater;
+import me.hsgamer.mcserverupdater.api.JenkinsUpdater;
 
 import java.util.regex.Pattern;
 
-public class MiraiUpdater extends GithubReleaseUpdater {
-    public MiraiUpdater() {
-        super("etil2jz/Mirai", true);
-    }
+public class MiraiUpdater extends JenkinsUpdater {
 
-    @Override
-    public Pattern getArtifactPattern(String version, String build) {
-        return Pattern.compile(".*\\.jar");
+    public MiraiUpdater() {
+        super("https://ci.codemc.io/");
     }
 
     @Override
     public String getDefaultVersion() {
-        return "1.18.2";
+        return "1.19";
+    }
+
+    @Override
+    public String[] getJob(String version) {
+        if (version.startsWith("1.19")) {
+            return new String[]{"etil2jz", "Mirai-1.19"};
+        }
+        return new String[0];
+    }
+
+    @Override
+    public Pattern getArtifactRegex(String version, String build) {
+        return Pattern.compile(".*\\.jar");
     }
 }
