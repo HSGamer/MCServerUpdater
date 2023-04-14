9af331fb035bfc63cb0dcd91beff22d368d6497b
HSGamer <huynhqtienvtag@gmail.com>
2023-04-14 18:50:40 +0700

    decouple the release fetcher on GithubReleaseUpdater

--- mc-server-updater-lib/src/main/java/me/hsgamer/mcserverupdater/api/GithubReleaseUpdater.java
+++ mc-server-updater-lib/src/main/java/me/hsgamer/mcserverupdater/api/GithubReleaseUpdater.java
@@ -19,18 +19,14 @@ public abstract class GithubReleaseUpdater implements SimpleChecksum, UrlInputSt
     protected final UpdateBuilder updateBuilder;
     protected final String version;
     protected final String build;
-    private final String repo;
-    private final boolean versionAsTag;
-    private final String releasesUrl;
-    private final String releaseByTagUrl;
+    protected final String releasesUrl;
+    protected final String repo;
     private final String releaseAssetUrl;
 
-    protected GithubReleaseUpdater(VersionQuery versionQuery, String repo, boolean versionAsTag) {
+    protected GithubReleaseUpdater(VersionQuery versionQuery, String repo) {
         this.repo = repo;
-        this.versionAsTag = versionAsTag;
         String url = "https://api.github.com/repos/" + repo + "/";
         this.releasesUrl = url + "releases";
-        this.releaseByTagUrl = url + "releases/tags/%s";
         this.releaseAssetUrl = url + "releases/%s/assets";
         this.updateBuilder = versionQuery.updateBuilder;
         this.version = versionQuery.isDefault ? getDefaultVersion() : versionQuery.version;
@@ -41,30 +37,35 @@ public abstract class GithubReleaseUpdater implements SimpleChecksum, UrlInputSt
 
     public abstract String getDefaultVersion();
 
-    private String getBuild() {
-        JSONObject object;
-        if (versionAsTag) {
-            String tagToIdUrl = String.format(releaseByTagUrl, version);
-            debug("Getting release ID from " + tagToIdUrl);
-            try {
-                URLConnection connection = UserAgent.CHROME.assignToConnection(WebUtils.createConnection(tagToIdUrl));
-                InputStream inputStream = connection.getInputStream();
-                object = new JSONObject(new JSONTokener(inputStream));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        } else {
-            String url = releasesUrl + "?per_page=1";
-            debug("Getting release ID from " + url);
-            try {
-                URLConnection connection = UserAgent.CHROME.assignToConnection(WebUtils.createConnection(url));
-                InputStream inputStream = connection.getInputStream();
-                JSONArray array = new JSONArray(new JSONTokener(inputStream));
-                object = array.getJSONObject(0);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+    public abstract JSONObject getReleaseObject();
+
+    protected JSONObject getLatestRelease() {
+        String url = releasesUrl + "?per_page=1";
+        debug("Getting release from " + url);
+        try {
+            URLConnection connection = UserAgent.CHROME.assignToConnection(WebUtils.createConnection(url));
+            InputStream inputStream = connection.getInputStream();
+            JSONArray array = new JSONArray(new JSONTokener(inputStream));
+            return array.getJSONObject(0);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+    }
+
+    protected JSONObject getReleaseByTag(String tag) {
+        String url = String.format(releasesUrl + "/tags/%s", tag);
+        debug("Getting release from " + url);
+        try {
+            URLConnection connection = UserAgent.CHROME.assignToConnection(WebUtils.createConnection(url));
+            InputStream inputStream = connection.getInputStream();
+            return new JSONObject(new JSONTokener(inputStream));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String getBuild() {
+        JSONObject object = getReleaseObject();
         if (object == null) {
             throw new RuntimeException("Cannot get release ID");
         }
--- mc-server-updater-lib/src/main/java/me/hsgamer/mcserverupdater/updater/TitaniumUpdater.java
+++ mc-server-updater-lib/src/main/java/me/hsgamer/mcserverupdater/updater/TitaniumUpdater.java
@@ -2,12 +2,13 @@ package me.hsgamer.mcserverupdater.updater;
 
 import me.hsgamer.mcserverupdater.api.GithubReleaseUpdater;
 import me.hsgamer.mcserverupdater.util.VersionQuery;
+import org.json.JSONObject;
 
 import java.util.regex.Pattern;
 
 public class TitaniumUpdater extends GithubReleaseUpdater {
     public TitaniumUpdater(VersionQuery versionQuery) {
-        super(versionQuery, "TitaniumMC/Titanium", false);
+        super(versionQuery, "TitaniumMC/Titanium");
     }
 
     @Override
@@ -18,5 +19,10 @@ public class TitaniumUpdater extends GithubReleaseUpdater {
     @Override
     public String getDefaultVersion() {
         return "1.8.8";
+    }
+
+    @Override
+    public JSONObject getReleaseObject() {
+        return getLatestRelease();
     }
 }
